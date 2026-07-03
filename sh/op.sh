342d8a1477b3bedb5812e97e192c1349680fc3cf
evol5201314 <46412120+evol5201314@users.noreply.github.com>
2026-07-03 18:19:42 +0800

    Update op.sh

--- sh/op.sh
+++ sh/op.sh
@@ -108,7 +108,12 @@ git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/l
 # 同步全部feeds源码
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
+##################################################
+# 2. 将 luci-app-commands 菜单从【系统】移动至【服务】分类
+sed -i 's/{"admin", "system", "commands"}/{"admin", "services", "commands"}/g' feeds/luci/applications/luci-app-commands/luasrc/controller/admin/commands.lua
+# 2. luci-app-nikki 从【网络】→【服务】分类
+sed -i 's/{"admin", "network", "nikki"}/{"admin", "services", "nikki"}/g' feeds/luci/applications/luci-app-nikki/luasrc/controller/admin/nikki.lua
+##################################################
 # ttyd免密root登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
